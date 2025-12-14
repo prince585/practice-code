cb3f3c45643b0b001f6ee2830256929b0ff90bcb
PrinceVerma <princeverma122345@gmail.com>
2025-12-14 18:34:43 +0530

    java file

---
+++ sam.java
@@ -0,0 +1,9 @@
+class main {
+
+    public static void main(String[] args) {
+        System.out.println("prince");
+        for (int i = 1; i < 6; i++) {
+            System.out.println("Prince Verma");
+        }
+    }
+}
